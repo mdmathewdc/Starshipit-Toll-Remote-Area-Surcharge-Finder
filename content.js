0b796cd613c5c4575e43b4211a33793ecd675542
mdmathewdc <mathewdony007@gmail.com>
2021-02-16 20:20:15 +1100

    Added content script

---
+++ content.js
@@ -0,0 +1,4 @@
+console.log("Content script working");
+
+var orderNumber = document.getElementById('ctl00_ContentPlaceHolder1_RadGrid_Orders_ctl00_ctl05_hl_id').innerHTML;
+console.log("The order number is : "+ orderNumber);
